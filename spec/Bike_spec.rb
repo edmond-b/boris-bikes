745eedb12ba2484d942ffb4a9f6699831ba91add
dpwdec <declankolakowski@gmail.com>
2020-04-15 14:42:21 +0000

    Added release_bike and working? methods and associated tests

--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -4,4 +4,10 @@ describe Bike do
   it 'bike responds ro working?' do
     expect(subject).to respond_to(:working?)
   end
+  
+  describe ".working?" do
+    it "returns true" do
+      expect(subject.working?).to be true
+    end
+  end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,7 +1,14 @@
 require 'DockingStation'
+require 'Bike'
 
 describe DockingStation do
   it 'responds to release_bike' do
     expect(subject).to respond_to(:release_bike)
+  end
+  
+  describe ".release_bike" do
+    it "returns a Bike object" do
+      expect(subject.release_bike).to be_a_kind_of(Bike)
+    end
   end
 end
